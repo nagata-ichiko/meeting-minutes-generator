9d292ff1e4a597059d2455c36cca4b04e1a67366
nagata-ichiko <eiichiro.kinoshita@i-standard.jp>
2023-10-25 17:39:45 +0900

    no message

--- main.py
+++ main.py
@@ -8,19 +8,20 @@ from pydub import AudioSegment
 import time
 from fastapi import FastAPI
 
-def excute(api_key, mp4_file_path,model):
-    print("処理を開始します...")
+
+pre_summary = ""
+output_folder = "./output/"
+def transcription_excute(api_key, mp4_file_path,model):
     openai.api_key = api_key
-    if model not in get_available_models(api_key):
-        return "エラー：使用できないモデルです。","エラー：使用できないモデルです。"
-    
+    transcription_list = []
+    print("処理を開始します...")    
     print("mp4ファイルをmp3に変換しています...")
     format = os.path.splitext(mp4_file_path.name.split("/")[-1])[1].replace(".", "")
     print(format)
     audio = AudioSegment.from_file(mp4_file_path.name, format=format)
     print("変換が完了しました。ファイルを作成します...")
 
-    output_folder = "./output/"
+
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
@@ -44,25 +45,37 @@ def excute(api_key, mp4_file_path,model):
         
     del audio
     print("文字起こしをしています...")
-    transcription_list = []
+
     for mp3_file_path in mp3_file_path_list:
         with open(mp3_file_path, 'rb') as audio_file:
             transcription = openai.Audio.transcribe("whisper-1", audio_file, language='ja')
         transcription_list.append(transcription.text)
 
-    pre_summary = ""
-    
+    result =""   
+
+    for i in transcription_list:
+        result += i + "\n"    
+    print("ファイルを削除します")
+    try:
+        os.remove(mp4_file_path.name)
+        for root, dirs, files in os.walk(output_folder):
+            for file in files:
+                os.remove(os.path.join(root, file))
+        
+    except Exception as e:
+        print(f"Error deleting original video file {mp4_file_path}: {e}")
+        
+    return result
+
+def pre_summary_excute(api_key,model,transcription,pre_summary_text):
+    print(transcription)
+    transcription_list = transcription.split("\n")
     print(transcription_list)
+    openai.api_key = api_key
+    pre_summary = ""
     for transcription_part in transcription_list:
         print("議事録を作成中です...")
-        prompt = """
-        あなたは、プロの要約作成者です。
-        以下の制約条件、内容を元に要点をまとめてください。
-
-        # 制約条件
-        ・要点をまとめ、簡潔に書いて下さい。
-        ・誤字・脱字があるため、話の内容を予測して置き換えてください。
-
+        prompt = pre_summary_text +"""
         # 内容
         """ + transcription_part
 
@@ -74,19 +87,15 @@ def excute(api_key, mp4_file_path,model):
             temperature=0.0,
         )   
         pre_summary += response['choices'][0]['message']['content']
+        
+    return pre_summary
 
-    prompt = """
-    あなたは、プロの議事録作成者です。
-    以下の制約条件、内容を元に要点をまとめ、議事録を作成してください。
-
-    # 制約条件
-    ・要点をまとめ、簡潔に書いて下さい。
-    ・誤字・脱字があるため、話の内容を予測して置き換えてください。
-    ・見やすいフォーマットにしてください。
-
+def summary_excute(api_key,model,pre_summary_out,summary_text):
+    openai.api_key = api_key
+    prompt = summary_text + """
     # 内容
-    """ + pre_summary
-    print(pre_summary)
+    """ + pre_summary_out
+    print(pre_summary_out)
     print("要約を作成中です...")
     response = openai.ChatCompletion.create(
         model=model,
@@ -95,26 +104,8 @@ def excute(api_key, mp4_file_path,model):
         ],
         temperature=0.0,
     )
-    print("処理が完了しました。")
-
-    result ="[文字起こし結果]"    
-    for i in transcription_list:
-        result += i + "\n"
-        
-    result += response['choices'][0]['message']['content']
-    
-    print("ファイルを削除します")
-    try:
-        os.remove(mp4_file_path.name)
-        for root, dirs, files in os.walk(output_folder):
-            for file in files:
-                os.remove(os.path.join(root, file))
-        
-    except Exception as e:
-        print(f"Error deleting original video file {mp4_file_path}: {e}")
-    
-    return result
-    # return  response['choices'][0]['message']['content']
+    print("処理が完了しました。")   
+    return  response['choices'][0]['message']['content']
     
 def get_available_models(api_key):
     openai.api_key = api_key
@@ -124,43 +115,59 @@ def get_available_models(api_key):
         if 'gpt' in model['id']:
             tempmodels.append(model['id'])
             
-    return tempmodels
-    
-models = [
-    'gpt-3.5-turbo-16k-0613',
-    'gpt-3.5-turbo-16k',
-    'gpt-4',
-    'gpt-4-0314',
-    'gpt-3.5-turbo-0613',
-    'gpt-3.5-turbo-instruct-0914',
-    'gpt-3.5-turbo-0301',
-    'gpt-3.5-turbo-instruct',
-    'gpt-3.5-turbo',
-    'gpt-4-0613'
-]
+    return gr.Dropdown.update(choices=tempmodels,interactive=True,value=tempmodels[0])
 
-meeting = gr.outputs.Textbox(label="議事録データ")
+
+
+
+
+transcription_out = gr.Textbox(label="文字起こし結果")
+presummary_prompt = """
+あなたは、プロの要約作成者です。
+以下の制約条件、内容を元に要点をまとめてください。
+# 制約条件
+・要点をまとめ、簡潔に書いて下さい。
+・誤字・脱字があるため、話の内容を予測して置き換えてください。     
+"""
+
+summary_prompt = """
+あなたは、プロの議事録作成者です。
+以下の制約条件、内容を元に要点をまとめ、議事録を作成してください。
+# 制約条件
+・要点をまとめ、簡潔に書いて下さい。
+・誤字・脱字があるため、話の内容を予測して置き換えてください。
+・見やすいフォーマットにしてください。
+・決まったこと、決めたことについては箇条書きで、明確に記載してください。省略は許しません。  
+"""
+
 
 with gr.Blocks() as inter:
     caption = gr.Markdown(
         """
-        ### 動画や音声を元に会議録を自動生成するためのアプリです。
-        ### 注意事項：このアプリは、OpenAIのAPIキーが必要です。APIキーを入力してください。
-        ### サーバーのメモリが少ないため、ファイルサイズに制限があります。40M以下のファイルを使用してください。(50Mまでは動作しましたが、8割がたサーバーが落ちます)。
-        ### 20Mくらいの音声ファイルがおすすめです。大きくても50Mくらい
-        ### 無料サーバーなのでちょっと重いです。反応をちょっとだけ待ってください。
-        ### ローカルで動かすとサクサクです。https://github.com/nagata-ichiko/meeting-minutes-generator
+        ##### 動画や音声を元に会議録を自動生成するためのアプリです。
+        ##### 注意事項：このアプリは、OpenAIのAPIキーが必要です。APIキーを入力してください。
+        ##### 音声ファイルがおすすめです。ファイルの容量は軽い方が良いです
         """
     )
+    with gr.Column():
+        api_key = gr.Textbox(label="APIキー")
+        api_button = gr.Button(value="APIキーを確認する", type="button")
+        api_list = gr.Dropdown(label="モデル", choices=None,interactive=True,value="APIキーを確認してください")
+        api_button.click(get_available_models, api_key,api_list)
     with gr.Row():
-        with gr.Column():
-            api_key = gr.inputs.Textbox(label="APIキー")
-            api_list = gr.inputs.Dropdown(label="モデル", choices=models)
-            file = gr.inputs.File(label="動画ファイル")
-            excute_Button = gr.Button(value="実行", type="button")
-            excute_Button.click(excute, [api_key, file, api_list], meeting)      
-        with gr.Row(scale=2):
-            meeting.render()
-
+        with gr.Row():
+            pre_summary_text =gr.Textbox(label="要約を作成する前処理用のプロンプト",interactive=True,value=presummary_prompt)
+            summary_text =gr.Textbox(label="要約を作成する用のプロンプト",interactive=True,value=summary_prompt)
+    with gr.Column():
+        file = gr.File(label="動画ファイル")
+        excute_Button = gr.Button(value="実行", type="button")    
+        excute_Button.click(transcription_excute, [api_key,file,api_list],transcription_out)
+    with gr.Column():
+        transcription_out.render()
+        pre_summary_out = gr.Textbox(label="前処理結果")
+        summary_out = gr.Textbox(label="議事録データ")
+        transcription_out.change(pre_summary_excute, [api_key,api_list,transcription_out,pre_summary_text],pre_summary_out)
+        pre_summary_out.change(summary_excute, [api_key,api_list,pre_summary_out,summary_text],summary_out)
+        
 app = FastAPI()
 app = gr.mount_gradio_app(app, inter,path="/")
